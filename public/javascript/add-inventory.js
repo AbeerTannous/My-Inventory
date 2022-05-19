41416d95655ffad336aadbc488719b9841c9a332
Abeer Tannous <{abeer.tannous}@yahoo.com>
2022-05-19 11:34:39 -0400

    create inventory logic

---
+++ public/javascript/add-inventory.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="inventory-title"]').value;
+    
+  
+    const response = await fetch(`/api/inventories`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title
+        
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-inventory-form').addEventListener('submit', newFormHandler);
